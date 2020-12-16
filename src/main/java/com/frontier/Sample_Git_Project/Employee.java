cddda1e6f8249cfd911dfdfb2e200c196f69d79b
StephenJ52 <StephenJ52@users.noreply.github.com>
2020-12-16 08:57:00 -0700

    added Employee getters and setters

--- src/main/java/com/frontier/Sample_Git_Project/Employee.java
+++ src/main/java/com/frontier/Sample_Git_Project/Employee.java
@@ -3,5 +3,22 @@ package com.frontier.Sample_Git_Project;
 public class Employee {
 	private String name;
 	private int age;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 
 }
+
