6f60878f76bca3d41187a3737295d60a5fc7631d
StephenJ52 <StephenJ52@users.noreply.github.com>
2020-12-15 19:13:28 -0700

    Initial Commit.

---
+++ src/main/java/com/frontier/Sample_Git_Project/App.java
@@ -0,0 +1,13 @@
+package com.frontier.Sample_Git_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
